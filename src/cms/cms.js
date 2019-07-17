025584400135593608b33b0980d759569e41657c
gurkodil <me@gustafwennerstrom.com>
2019-07-17 19:12:55 +0200

    removed dependencie

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -9,7 +9,7 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 
 CMS.registerMediaLibrary(uploadcare);
-CMS.registerMediaLibrary(cloudinary);
+// CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
