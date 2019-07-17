d7ea5db830a0025e23e24e5fef3cd3c234885460
gurkodil <me@gustafwennerstrom.com>
2019-07-18 01:34:38 +0200

    Fixed graphql on indexpage

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -154,34 +154,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
+        video 
       }
     }
   }
