dd160c486573e56a7e27abf0d45a7fd1c2e6a161
gurkodil <me@gustafwennerstrom.com>
2019-08-01 13:36:24 +0200

    some minor css-styling for smaller screens

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,90 +9,91 @@ import "aos/dist/aos.css"
 
 class HomePageGrid extends React.Component {
 
-    constructor(props) {
-        super(props)
-        const { data } = props
-        const { allMarkdownRemark } = data
+  constructor(props) {
+    super(props)
+    const { data } = props
+    const { allMarkdownRemark } = data
 
-        this.aos = AOS
+    this.aos = AOS
 
-        /**
-         * Get all unsorted projects
-         */
-        const gridItems = allMarkdownRemark.edges
-            .map(edge => {
-                const { slug } = edge.node.fields
-                const { thumbnail, title } = edge.node.frontmatter
-                const { gridPosition } = edge.node.fields
-            
-                return ({
-                    title,
-                    thumbnail: thumbnail.childImageSharp,
-                    slug,
-                    gridPosition,
-                })
-            })
-            .sort((a, b) => a.gridPosition > b.gridPosition)
+    /**
+     * Get all unsorted projects
+     */
+    const gridItems = allMarkdownRemark.edges
+      .map(edge => {
+        const { slug } = edge.node.fields
+        const { thumbnail, title } = edge.node.frontmatter
+        const { gridPosition } = edge.node.fields
 
-        this.state = {
-            gridItems,
-            windowInitialized: false
-        }
-    }
-
-    componentDidMount() {
-        this.aos.init({
-            offset: 20,
-            duration: 1550,
+        return ({
+          title,
+          thumbnail: thumbnail.childImageSharp,
+          slug,
+          gridPosition,
         })
+      })
 
-        this.setState({
-            windowInitialized: true
-        })
+    this.state = {
+      gridItems,
+      windowInitialized: false
     }
+  }
 
-    getWindowDimensions() {
-        const { innerWidth: width, innerHeight: height } = window
-        return {
-            width,
-            height,
-        }
+  componentDidMount() {
+    this.aos.init({
+      offset: 20,
+      duration: 1550,
+    })
+
+    this.setState({
+      windowInitialized: true
+    })
+  }
+
+  getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window
+    return {
+      width,
+      height,
     }
+  }
 
-    componentWillReceiveProps() {
-        this.aos.refresh()
-    }
+  componentWillReceiveProps() {
+    this.aos.refresh()
+  }
 
-    render() {
-        if (!this.state.windowInitialized) return null
-        const { gridItems } = this.state
-        const { width } = this.getWindowDimensions()
+  render() {
+    if (!this.state.windowInitialized) return null
+    const { gridItems } = this.state
+    const { width } = this.getWindowDimensions()
 
-        const cols = width < 770 ? 2 : 3
-        const gap = cols === 3 ? 5 : 3
+    const cols = width < 770 ? 2 : 3
+    const gap = cols === 3 ? 5 : 3
 
-        return (
-            <Layout>
-                <GridLayout columns={cols} gap={gap} className="grid">
-                    {gridItems.map((gridItem, index) =>
-                        <figure data-aos="fade-up" onClick={() => navigate(gridItem.slug)} className={'gridItem'}
-                            key={`gridItemKey${index}`}
-                        >
-                            <Img style={{ width: '100%' }} fluid={gridItem.thumbnail.fluid} alt={gridItem.title} />
-                            <figcaption>{gridItem.title}</figcaption>
-                        </figure>
-                    )}
-                </GridLayout>
-            </Layout >
-        )
-    }
+    return (
+      <Layout>
+        <GridLayout columns={cols} gap={gap} className="grid">
+          {gridItems.map((gridItem, index) =>
+            <figure data-aos="fade-up" onClick={() => navigate(gridItem.slug)} className={'gridItem'}
+              key={`gridItemKey${index}`}
+            >
+              <Img style={{ width: '100%' }} fluid={gridItem.thumbnail.fluid} alt={gridItem.title} />
+              <figcaption>{gridItem.title}</figcaption>
+            </figure>
+          )}
+        </GridLayout>
+      </Layout >
+    )
+  }
 }
 
 export default HomePageGrid
 
 export const allProjectsQuery = graphql`
 query queryGrid {
-    allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "projects-page"}}}) {
+    allMarkdownRemark(filter: {
+        frontmatter: {templateKey: {eq: "projects-page"}}}
+        sort: { fields: fields___gridPosition order: ASC }) {
       edges {
         node {
           fields {
